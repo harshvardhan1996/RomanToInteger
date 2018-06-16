7d6a51b93135b160e3b4791124a04e6b271ecf29
harshvardhan1996 <harshamazon1996@gmail.com>
2018-06-16 22:09:37 +0530

    Converting Roman to Integer

--- RomanToInteger/Program.cs
+++ RomanToInteger/Program.cs
@@ -10,6 +10,40 @@ namespace RomanToInteger
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the Roman Numeral");
+            string s = Console.ReadLine();
+            Console.WriteLine("The Integer for this Roman is : {0}", RomanToInt(s));
+            Console.ReadKey();
+        }
+        public static int RomanToInt(string s)
+        {
+            int sum = 0;
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            Dictionary<char, int> dict = new Dictionary<char, int>()
+            {
+               {'I', 1},
+               {'V', 5},
+               {'X', 10},
+               {'L', 50},
+               {'C', 100},
+               {'D', 500},
+               {'M', 1000}
+            };
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (dict[s[i]] < dict[s[i + 1]])
+                    sum = sum - dict[s[i]];
+                else
+                    sum = sum + dict[s[i]];
+            }
+            sum += dict[s[s.Length - 1]];
+
+            return sum;
         }
     }
 }
